5cba2f48c82138a57a561eb7db3e6367a4fcdbb9
Sebastian Martinez Lopez <sebastian.martinez7643@alumnos.udg.mx>
2022-05-14 04:26:03 -0500

    first commit

---
+++ resources/views/cursos/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Cursos')
+@section('content')
+    <h1>En esta página podras crear un curso</h1>
+@endsection
---
+++ resources/views/cursos/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Cursos')
+@section('content')
+    <h1>Bienvenido a la página principal de cursos</h1>  
+@endsection
